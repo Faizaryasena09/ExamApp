85514a377468e85d028343357b7e0d4e8c8fbe75
Faizaryasena09 <cenax09@gmail.com>
2025-08-30 13:29:53 +0700

    th

--- frontend/RushlessSafer/obj/Debug/net8.0-windows/RushlessSafer.AssemblyInfo.cs
+++ frontend/RushlessSafer/obj/Debug/net8.0-windows/RushlessSafer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RushlessSafer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+299690d312515c632e6fdbb97ea1d05fcb96c3b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae25e205d671d2fff9d99c562ae92e384180ee1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("RushlessSafer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RushlessSafer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
